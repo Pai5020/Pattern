5e67ba0b3154ae30e75f3c8b28dddb71d754ee3c
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-04-06 21:49:52 +0530

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] pn = { "*","*","***","**","**","******","***","***","***","*********" };
+            for (int i = 0; i < pn.length; i++) {
+                System.out.println(pn[i]);
+            }
+            System.out.println();
+        }
+    }
